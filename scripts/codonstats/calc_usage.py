aa1abaeec8af3dcf41cfcb62d01fd3eb241ea5bc
szabogtamas <szabogtamas@gmail.com>
2022-01-11 20:46:42 +0100

    Moved codon freq calc function to script

--- scripts/codonstats/calc_usage.py
+++ scripts/codonstats/calc_usage.py
@@ -1,4 +1,25 @@
-import numpy as np
+from Bio import SeqIO
 
-def codon_tab(seq):
-    return
+import numpy as np
+import pandas as pd
+
+import os, itertools
+
+def calc_codon_freq_of_cds(
+    seq,
+    proto_d={"".join(x): 0 for x in itertools.product("CUAG", repeat=3)},
+    complement=True
+):
+    c = 0
+    d = proto_d.copy()
+    if complement:
+        seq = seq.reverse_complement()
+    for i in range(0, len(seq), 3):
+        c += 1
+        codon = str(seq[i:i+3])
+        try:
+            d[codon] += 1
+        except:
+            pass
+    d["SUM"] = c
+    return d
